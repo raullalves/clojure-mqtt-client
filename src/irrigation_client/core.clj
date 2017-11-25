255df210355fdbb2c2409084f6a362cd5616a216
Raul Lima Alves <raullalves@gmail.com>
2017-11-25 10:43:58 -0200

    new connection method

--- src/irrigation_client/core.clj
+++ src/irrigation_client/core.clj
@@ -1,11 +1,17 @@
 (ns irrigation-client.core
   (:gen-class)
   (:require [clojurewerkz.machine-head.client :as mh]))
+(def ^:const topic-send-info "irrigathon-send")
+(def ^:const topic-receive-action "irrigathon-receive")
+(defn start-receive-action
+  [conn]
+  (mh/subscribe conn {topic-receive-action 0} (fn [^String topic _ ^bytes payload]
+                                   (println (String. payload "UTF-8"))))
+)
 (defn -main
   [& args]
   (let [conn (mh/connect "tcp://127.0.0.1:1883")]
-    (mh/subscribe conn {"hackaton" 0} (fn [^String topic _ ^bytes payload]
-                                   (println (String. payload "UTF-8"))
-                                   (mh/disconnect conn)
-                                   (System/exit 0)))
-    (mh/publish conn "hackaton" "longitude, latitude")))
+    (start-receive-action conn)
+    (mh/publish conn topic-receive-action "longitude, latitude")
+    (Thread/sleep 100)
+    (mh/disconnect conn)))
