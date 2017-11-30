bd484089c06356031e16d1ee562e3d186d74d6fb
Raul Lima Alves <raullalves@gmail.com>
2017-11-30 12:05:21 -0200

    new client

--- src/irrigation_client/core.clj
+++ src/irrigation_client/core.clj
@@ -1,22 +1,16 @@
 (ns irrigation-client.core
   (:gen-class)
   (:require [clojurewerkz.machine-head.client :as mh]))
-(def ^:const topic-send-info "irrigathon/lonlat")
-(def ^:const topic-receive-action "irrigathon/action")
-(defn start-receive-action
+(def ^:const topic "mqtt/hackaton/enviar/id1")
+(def ^:const topicReceber "mqtt/hackaton/receber/id1")
+(defn start
   [conn]
-  (mh/subscribe conn {topic-receive-action 0} (fn [^String topic _ ^bytes payload]
-                                   (println (String. payload "UTF-8"))))
-)
-(defn start-send-info
-  [conn]
-  (mh/subscribe conn {topic-send-info 0} (fn [^String topic _ ^bytes payload]
+  (mh/subscribe conn {topicReceber 0} (fn [^String topic _ ^bytes payload]
                                    (println (String. payload "UTF-8"))))
 )
 (defn -main
   [& args]
   (let [conn (mh/connect "tcp://127.0.0.1:1883")]
-    (start-receive-action conn)
-    (mh/publish conn topic-receive-action "longitude, latitude")
-    (Thread/sleep 100)
-    (mh/disconnect conn)))
+    (start conn)
+    (mh/publish conn topic "-23.5505199;-46.6333094")
+    ))
